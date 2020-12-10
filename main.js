26ca1c0758edc42176e8e27f0a19bd174e4cb4a0
ohampton <44102431+ohampton@users.noreply.github.com>
2020-12-10 09:54:22 -0600

    commit

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 function toCelsius() {
-    let conFarenheit = document.getElementById("farenheitInput").value;
+    let conFarenheit = document.getElementById("userInput").value;
     let finalcelsius = conFarenheit * .555555556;
     
 } 
+
+function toFarenheit() {
+    let conCelsius = document.getElementById("userInput").value;
+    let finalcelsius = conCelsius * .555555556;
+    
+} 
 console.log(toCelsius);
+
+
+
+
+
+
