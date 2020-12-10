7af6da3687ac4f9b9d6cc4069b71e81a1af97e80
ohampton <44102431+ohampton@users.noreply.github.com>
2020-12-10 12:31:00 -0600

    delete comment

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// function toCelsius() {
-//     let conFarenheit = document.getElementById("userInput").value;
-//     let finalcelsius = conFarenheit * .555555556;
-    
-// } 
-
-// function toFarenheit() {
-//     let conCelsius = document.getElementById("userInput").value;
-//     let finalcelsius = conCelsius * .555555556;
-    
-// } 
-
-
-// console.log(toCelsius);
-
 function selectUnit() {
     let userOutput;
     let isChecked = document.getElementById("farenheitTemp").checked
