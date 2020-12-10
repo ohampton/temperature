a95abf014f1469ee787d479e8f1dd0fe0d0b6efb
ohampton <44102431+ohampton@users.noreply.github.com>
2020-12-10 12:30:12 -0600

    fix JS

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-function toCelsius() {
-    let conFarenheit = document.getElementById("userInput").value;
-    let finalcelsius = conFarenheit * .555555556;
+// function toCelsius() {
+//     let conFarenheit = document.getElementById("userInput").value;
+//     let finalcelsius = conFarenheit * .555555556;
     
-} 
+// } 
 
-function toFarenheit() {
-    let conCelsius = document.getElementById("userInput").value;
-    let finalcelsius = conCelsius * .555555556;
+// function toFarenheit() {
+//     let conCelsius = document.getElementById("userInput").value;
+//     let finalcelsius = conCelsius * .555555556;
     
-} 
+// } 
 
 
-console.log(toCelsius);
+// console.log(toCelsius);
 
+function selectUnit() {
+    let userOutput;
+    let isChecked = document.getElementById("farenheitTemp").checked
+    let tempInput = document.getElementById("userInput").value 
+    if (isChecked == true) {
 
+        userOutput = (5/9) * (tempInput - 32);
+
+    }
+
+    else {
+        userOutput = (tempInput * (9/5) + 32);
+    }
+    document.getElementById("userOutput").value = userOutput;
+}
 
 
 
